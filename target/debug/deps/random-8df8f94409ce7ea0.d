1a9e3361d8261c4774a3e40a618b3e67cbf241c4
nbari <nbari@tequila.io>
2025-09-28 16:37:26 +0200

    bump version to 0.1.1

---
+++ target/debug/deps/random-8df8f94409ce7ea0.d
@@ -0,0 +1,5 @@
+/home/nbari/projects/git/target/debug/deps/random-8df8f94409ce7ea0.d: src/main.rs
+
+/home/nbari/projects/git/target/debug/deps/random-8df8f94409ce7ea0: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/random-fd7e92810a6b7c3d.d
@@ -0,0 +1,9 @@
+/home/nbari/projects/git/target/debug/deps/random-fd7e92810a6b7c3d.d: src/main.rs Cargo.toml
+
+/home/nbari/projects/git/target/debug/deps/librandom-fd7e92810a6b7c3d.rmeta: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
