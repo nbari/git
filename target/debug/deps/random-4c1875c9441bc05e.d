24eacc70c63619de5b6b46bad3394058e8ddd796
nbari <nbari@tequila.io>
2025-09-28 16:39:45 +0200

    sync Sun Sep 28 04:39:45 PM CEST 2025

---
+++ target/debug/deps/random-4c1875c9441bc05e.d
@@ -0,0 +1,5 @@
+/home/nbari/projects/git/target/debug/deps/random-4c1875c9441bc05e.d: src/main.rs
+
+/home/nbari/projects/git/target/debug/deps/random-4c1875c9441bc05e: src/main.rs
+
+src/main.rs:
---
+++ target/debug/random.d
@@ -0,0 +1,1 @@
+/home/nbari/projects/git/target/debug/random: /home/nbari/projects/git/src/main.rs
